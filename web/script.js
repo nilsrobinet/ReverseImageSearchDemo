af3c9e5a2f6782fe0a70c85d32472d8dad419328
Nils Robinet <nils.robinet@gmail.com>
2024-04-28 14:51:41 +0200

    Add basic styling to web frontend

--- web/script.js
+++ web/script.js
@@ -16,19 +16,16 @@ function search () {
             img.src=resp[0][0].id
             img.id="picture"
             img.width = 400
-            img.height = 300
             document.getElementById("result_1").appendChild(img);
             var img=document.createElement("img");
             img.src=resp[0][1].id
             img.id="picture"
             img.width = 400
-            img.height = 300
             document.getElementById("result_2").appendChild(img);
             var img=document.createElement("img");
             img.src=resp[0][2].id
             img.id="picture"
             img.width = 400
-            img.height = 300
             document.getElementById("result_3").appendChild(img);
         } else {
             console.log(`Error: ${xhr.status}`);
